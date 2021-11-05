873ba39e2114dc5af70ee13772d2fe09fe079c7f
AntowanG-tech <76835658+AntowanG-tech@users.noreply.github.com>
2021-11-04 23:27:46 -0700

    Milestone 3 commit.
    InventoryManager class > search, restock, and create do not work. Could be better as a list. Not array.

---
+++ InventoryManager.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST_150Milestone3
+{
+    class InventoryManager
+    {
+        Blanket[] blankets = new Blanket[5];
+
+        public void createBlanket(int id, string type, string shape, string color, string material, string size, double price)
+        {
+            //Method will create a blanket object to be placed in array
+            Blanket newBlanket = new Blanket(id, type, shape, color, material, size, price);
+            bool ifBlanketWasAssigned = false;
+
+            for (int i = 0; i < blankets.Length; i++)
+            {
+                if ( blankets[i] == null)
+                {
+                    blankets[i] = newBlanket;
+                    ifBlanketWasAssigned = true;
+                    break;
+                }
+            }
+
+            if(ifBlanketWasAssigned == false)
+            {
+                Console.WriteLine("Blanket could not be populated in the array.");
+            }
+
+
+        }
+
+        public void removeBlanket(int blanketID) 
+        {
+            Console.WriteLine(blanketID);
+            
+             blankets = blankets.Where((source, index) => index != blanketID).ToArray();
+
+            for (int i = 0; i < blankets.Length; i++)
+            {
+                Console.WriteLine(blankets[i].ToString());
+            }
+
+        }
+
+        public void restockBlankets()
+        {
+            if (blankets[0] == null) 
+            {
+                blankets[0] = new Blanket(0, "quilt", "circle", "grey", "fleece", "72x72", 70.00d);
+            }
+            if (blankets[1] == null)
+            {
+                blankets[1] = new Blanket(1, "quilt", "square", "dark blue", "cotton", "72x72", 55.00d);
+            }
+            if (blankets[2] == null)
+            {
+                blankets[2] = new Blanket(2, "throw", "rectangle", "pink", "wool", "72x72", 60.00d);
+            }
+            if (blankets[3] == null)
+            {
+                blankets[3] = new Blanket(3, "comforter", "square", "brown", "cotton", "72x72", 48.00d);
+            }
+            if (blankets[4] == null)
+            {
+                blankets[4] = new Blanket(4, "quilt", "star", "dark green", "polyester", "32x72x43", 55.00d);
+            }
+        }
+
+        public Blanket[] getBlankets()
+        {
+            
+
+            return blankets;
+        }
+
+        public Blanket[] searchForBlanket(string type, string shape)
+        {
+            Blanket[] blanketSearchArray = new Blanket[blankets.Length];
+                for (int i = 0; i < blankets.Length; i++)
+                {
+                if (blankets[i] != null && blankets[i].Type != null && blankets[i].Type.Contains(type))
+                {
+                    blanketSearchArray[i] = blankets[i];
+                }
+                if (blankets[i] != null && blankets[i].Shape != null && blankets[i].Shape.Contains(shape))
+                {
+                    blanketSearchArray[i] = blankets[i];
+                }
+            }
+            return blanketSearchArray;
+        }
+
+           
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST_150Milestone3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InventoryManager inventory = new InventoryManager();
+            Blanket[] blanketsArray = inventory.getBlankets();
+
+            inventory.restockBlankets();
+            
+           
+            Console.WriteLine("Press 'x' to close: ");
+            Console.WriteLine("Press 'y' to create a blanket: ");
+            Console.WriteLine("Press 'z' to display all blankets: ");
+            Console.WriteLine("Press 'e' to  search a blanket: ");
+            Console.WriteLine("Press 'r' to  remove a blanket: ");
+            Console.WriteLine("Press 't' to restock blankets: ");
+            Console.Write("Please enter a command: ");
+            String input = Console.ReadLine();
+            
+            while (input != "x")
+            {
+                if (input == "y")
+                {
+                    Console.Write("Enter blanket id: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter blanket type: ");
+                    String type = Console.ReadLine();
+                    Console.Write("Enter blanket shape: ");
+                    String shape = Console.ReadLine();
+                    Console.Write("Enter blanket color: ");
+                    String color = Console.ReadLine();
+                    Console.Write("Enter blanket material: ");
+                    String material = Console.ReadLine();
+                    Console.Write("Enter blanket size: ");
+                    String size = Console.ReadLine();
+                    Console.Write("Enter blanket price: ");
+                    double price = Convert.ToDouble(Console.ReadLine());
+                    inventory.createBlanket(id, type, shape, color, material, size, price);
+                }
+                else if (input == "z")
+                {
+                    for (int i = 0; i < blanketsArray.Length; i++)
+                    {
+                        Console.WriteLine(blanketsArray[i].ToString());
+                    }
+                }
+                else if ( input == "e")
+                {
+                    Console.WriteLine("Enter a blanket type: ");
+                    String type = Console.ReadLine();
+                    Console.WriteLine("Enter a blanket shape: ");
+                    String shape = Console.ReadLine();
+                    Blanket [] searchArray = inventory.searchForBlanket(type, shape);
+                    for (int i = 0; i < searchArray.Length; i++)
+                    {
+                        Console.WriteLine(searchArray[i].ToString());
+                    }
+
+                }
+                else if ( input == "r")
+                {
+                    Console.Write("Enter blanket position to remove: ");
+                    int position = Convert.ToInt32(Console.ReadLine());
+                    inventory.removeBlanket(position);
+                }
+                else if (input == "t")
+                {
+                    inventory.restockBlankets();
+                    Console.WriteLine("Inventory has been restocked");
+                }
+                else
+                {
+                    Console.WriteLine("Input not recognized.");
+                }
+
+                Console.WriteLine("Press 'x' to close: ");
+                Console.WriteLine("Press 'y' to create a blanket: ");
+                Console.WriteLine("Press 'z' to display all blankets: ");
+                Console.WriteLine("Press 'e' to  search a blanket: ");
+                Console.WriteLine("Press 'r' to  remove a blanket: ");
+                Console.WriteLine("Press 't' to restock blankets: ");
+                Console.Write("Please enter a command: ");
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
